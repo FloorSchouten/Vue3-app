269416d40567ffcece8bfa1f320ab4545cd76824
Floor Schouten <corporatecontent@outlook.com>
2021-03-17 11:59:21 +0100

    Lesson 5 Vue3

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ const app = Vue.createApp({
             // url: 'www.telegraaf.nl',
             // inStock: false
             inventory: 11,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}
+                ],
+            sizes: ['S', 'M','L'],
+
         }
     }
 })
