b04751c5a0bff979bb68b0e8690e6839b77f0a47
Floor Schouten <corporatecontent@outlook.com>
2021-03-18 11:55:56 +0100

    Setup pages in app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Vue3-app
+# Cowmanager
--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            
-        }
+            menu: [
+                {
+                    href: '/Home',
+                    title: 'Dashboard',
+                },
+                {
+                    href: '/About',
+                    title: 'About',
+                },
+                {
+                    href: '/Contact',
+                    title: 'Contact'
+                }
+            ]
+        
     },
         methods: {
             
